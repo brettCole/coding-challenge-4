1cfc169a2e35e92f9bf4b43a0bbf2c62ed7644e5
github username <github email address>
2018-04-16 22:56:08 -0400

    first commit

---
+++ challenge.js
@@ -0,0 +1,37 @@
+const fs = require('fs');
+
+fs.readFile('./puzzleInput.txt', (err, data) => {
+  let j = data.length;
+  let i = 0;
+  let increment = 0;
+  if (err) {
+    console.log(err)
+  }
+  data = data.toString().split('');
+  
+  console.time('testing');
+  while(j--) {
+    if(data[i] === '(') {
+      increment++;
+    }
+    else {
+      increment--;
+    }
+    i++;
+  }
+  console.log(increment);
+  console.timeEnd('testing');
+});
+
+// function whichLevel(input) {
+//   let increment = 0;
+//   for (let i = 0; i < input.length; i++) {
+//     if (input[i] == '(') {
+//       increment += 1;
+//     }
+//     if (input[i] == ')') {
+//       increment -= 1;
+//     }
+//   }
+//   return increment;
+// }
